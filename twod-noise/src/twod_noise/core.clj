e96ca923dbb3a2b7c610ec1bd1712576bf9eca6b
bberak <bberak@gmail.com>
2015-11-24 10:01:36 +1100

    Refactoring 2D noise exercise

--- twod-noise/src/twod_noise/core.clj
+++ twod-noise/src/twod_noise/core.clj
@@ -3,51 +3,28 @@
             [quil.middleware :as m]))
 
 (defn setup []
-  ; Set frame rate to 30 frames per second.
+  (q/background 255)
   (q/frame-rate 30)
-  ; setup function returns initial state
   {:t 0})
 
 (defn update-state [state]
-  {:t (+ (:t state) 0.01)})
-
-(defn get-pixel-brightness [x y t]
-  (q/map-range (q/noise (* x 0.01) (* y 0.01) t) 0 1 0 255))
-
-(defn set-pixel [pixels pixel-idx brightness]
-  (aset-int pixels pixel-idx (q/color brightness)))
+  {:t (+ (:t state) 1)})
 
 (defn draw-state [state]
-  ; Clear the sketch by filling it with light-grey color.
-  (q/background 255)
-  ; Update each pixel with a 2D noise value
-  (let [max-rows (q/height)
-        max-cols (q/width)
-        pixels (q/pixels)
+  (let [pixels (q/pixels)
         t (:t state)]
-    (loop [row 0]
-      (loop [col 0]
-        (let [pixel-idx (+ (* row max-cols) col)
-              brightness (get-pixel-brightness col row t)]
-          (set-pixel pixels pixel-idx brightness))
-        (if (< col (dec max-cols))
-          (recur (inc col))))
-      (if (< row (dec max-rows))
-        (recur (inc row)))))
-  ; Update the pixels with changes
+    (doseq [x (range 0 (q/width))
+            y (range 0 (q/height))]
+      (let [brightness (q/map-range (q/noise (* x 0.025) (* y 0.025) (* t 0.025)) 0 1 0 255)]
+        (aset-int pixels (+ x (* y (q/width))) (q/color brightness)))))
   (q/update-pixels))
 
 (defn -main []
   (q/defsketch twod-noise
-    :title "You spin my circle right round"
+    :title "2D Noise - Smokey"
     :size [640 480]
-    ; setup function called only once, during sketch initialization.
     :setup setup
-    ; update-state is called on each iteration before draw-state.
     :update update-state
     :draw draw-state
     :features [:keep-on-top]
-    ; This sketch uses functional-mode middleware.
-    ; Check quil wiki for more info about middlewares and particularly
-    ; fun-mode.
     :middleware [m/fun-mode]))
