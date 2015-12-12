fc30f0e673c0a66261919041ac2c986b243136e7
Boris Berak <bberak@gmail.com>
2015-12-12 13:47:24 +1100

    Continued work on vectors and lines

--- vector-line/src/vector_line/core.clj
+++ vector-line/src/vector_line/core.clj
@@ -4,23 +4,31 @@
 
 (defn setup []
   (q/frame-rate 60)
-  {:origin {:x (/ (q/width) 2)
-            :y (/ (q/height) 2)}
-   :mouse {:x (/ (q/width) 2)
-           :y (/ (q/height) 2)}})
+  {:mouse {:x 0
+           :y 0}})
+
+(defn multiply-vector [v n]
+  {:x (* (:x v) n)
+   :y (* (:y v) n)})
+
+(defn subtract-vectors [v1 v2]
+  {:x (- (:x v1) (:x v2))
+   :y (- (:y v1) (:y v2))})
 
 (defn update-state [state]
-  {:origin {:x (/ (q/width) 2)
-            :y (/ (q/height) 2)}
-   :mouse {:x (q/mouse-x)
+  {:mouse {:x (q/mouse-x)
            :y (q/mouse-y)}})
 
 (defn draw-state [state]
   (q/background 240)
-  (q/line (get-in state [:origin :x])
-          (get-in state [:origin :y])
-          (get-in state [:mouse :x])
-          (get-in state [:mouse :y])))
+  (let [center {:x (/ (q/width) 2) :y (/ (q/height) 2)}
+        scale 0.5
+        mouse (multiply-vector (subtract-vectors (:mouse state) center) scale)]
+    (q/translate (:x center) (:y center))
+    (q/line 0
+            0
+            (:x mouse)
+            (:y mouse))))
 
 (defn -main []
   (q/defsketch vector-line
