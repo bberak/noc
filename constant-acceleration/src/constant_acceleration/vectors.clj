1c9450b262f7b8046a073f790049e9286da431da
bberak <bberak@gmail.com>
2015-12-13 17:33:55 +1100

    Added constant-acceleration example

---
+++ constant-acceleration/src/constant_acceleration/vectors.clj
@@ -0,0 +1,36 @@
+(ns constant-acceleration.vectors
+  (:require [quil.core :as q]))
+
+(defn -vector-ops [op {x1 :x y1 :y} {x2 :x y2 :y}]
+  {:x (op x1 x2)
+   :y (op y1 y2)})
+
+(defn add [v1 v2]
+  (-vector-ops + v1 v2))
+
+(defn subtract [v1 v2]
+  (-vector-ops - v1 v2))
+
+(defn multiply [{x :x y :y} n]
+  {:x (* x n)
+   :y (* y n)})
+
+(defn divide [{x :x y :y} n]
+  (if (= n 0)
+    {:x x
+     :y y}
+	{:x (/ x n)
+	 :y (/ y n)}))
+
+(defn magnitude [{x :x y :y}]
+  (q/sqrt (+ (q/pow x 2) (q/pow y 2))))
+
+(defn normalize [v]
+  (let [mag (magnitude v)]
+    (divide v mag)))
+
+(defn limit [v n]
+  (let [mag (magnitude v)]
+    (if (> mag n)
+      (multiply (normalize v) n)
+      v)))
