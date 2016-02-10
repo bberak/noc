6fd572eaa05fdf0aad38b3122729f7fd7559793f
bberak <bberak@gmail.com>
2016-02-10 17:26:39 +1100

    Continued work on the air and fluid resistance example

--- air-and-fluid-resistance/src/air_and_fluid_resistance/core.clj
+++ air-and-fluid-resistance/src/air_and_fluid_resistance/core.clj
@@ -3,61 +3,78 @@
             [quil.middleware :as m]
             [air-and-fluid-resistance.vectors :as v]))
 
+(defn log [anything]
+  (println anything)
+  anything)
+
 (defn create-ball []
   (let [width (q/width)
         height (q/height)
         mass (q/random 1 5)
         radius (* mass 16)]
-    {:location (v/create (q/random 0 (q/width)) (q/random 0 (q/height)))
+    {:location (v/create (q/random 0 (q/width)) (q/random 0 0))
      :velocity (v/create (q/random -10 10) (q/random -10 10))
      :radius radius
      :max-speed 10
-     :mass mass}))
+     :mass mass
+     :surface-area 2.0}))
 
 (defn create-liquid []
   {:location (v/create 0 (/ (q/height) 2))
-   :size (v/create (q/width) (q/height))
    :bounds {:x {:min 0 :max (q/width)} :y {:min (/ (q/height) 2) :max (q/height)}}
-   :drag-coefficient 4.0})
+   :drag-coefficient 0.1
+   :density 2.0})
 
 (defn setup []
   (q/frame-rate 30)
   {:balls (doall (map (fn [x] (create-ball)) (range 20)))
    :forces {:wind (v/create 0.01 0)
-            :gravity (v/create 0 0.1)}})
+            :gravity (v/create 0 0.1)}
+   :liquid (create-liquid)})
 
-(defn log [anything]
-  (println anything)
-  anything)
+(defn drag [ball liquid]
+  (if (v/within-bounds? (:location ball) (get-in liquid [:bounds :x]) (get-in liquid [:bounds :y]))
+    (let [velocity (:velocity ball)
+          drag-coefficient (:drag-coefficient liquid)
+          speed (v/magnitude velocity)
+          liquid-density (:density liquid)
+          surface-area (:surface-area ball)
+          normalized-velocity (v/normalize velocity)]
+      ;; Simplified drag model: (v/multiply normalized-velocity (* -1 (* drag-coefficient (q/sq speed))))
+      (v/multiply normalized-velocity (* (* (* -0.5 (* liquid-density (q/sq speed))) surface-area) drag-coefficient)))
+    {:x 0 :y 0}))
 
-(defn calculate-friction [velocity]
-  (let [normal-magnitude 1.0 ;; Usually, this is calculated based on the object's velocity and gravity
-        friction-coefficient 0.21 ;; The higher, the more coarse (or resistant) the material is
-        normalized-velocity (v/normalize velocity)]
-    (v/multiply normalized-velocity (* -1 normal-magnitude friction-coefficient))))
-
-(defn update-ball [{location :location velocity :velocity radius :radius max-speed :max-speed mass :mass} 
-                   {wind :wind gravity :gravity}]
-  (let [acceleration (v/divide (v/add 
-                                  wind 
-                                  (v/multiply gravity mass)
-                                  (calculate-friction velocity))
+(defn update-ball [ball forces liquid]
+  (let [mass (:mass ball)
+        max-speed (:max-speed ball)
+        acceleration (v/divide (v/add 
+                                  (:wind forces) 
+                                  (v/multiply (:gravity forces) mass)
+                                  (drag ball liquid))
                       mass)
-        new-velocity (v/constrain-magnitude (v/add velocity acceleration) max-speed)
-        new-location (v/add location new-velocity)
+        new-velocity (v/constrain-magnitude (v/add (:velocity ball) acceleration) max-speed)
+        new-location (v/add (:location ball) new-velocity)
         bounce-results (v/bounce new-location new-velocity)]
     {:location (:location bounce-results)
      :velocity (:velocity bounce-results)
-     :radius radius
+     :radius (:radius ball)
      :max-speed max-speed
-     :mass mass}))
+     :mass mass
+     :surface-area (:surface-area ball)}))
 
-(defn update-state [{balls :balls forces :forces}]
-  {:balls (doall (map (fn [b] (update-ball b forces)) balls))
-   :forces forces})
+(defn update-state [{balls :balls forces :forces liquid :liquid}]
+  {:balls (doall (map (fn [b] (update-ball b forces liquid)) balls))
+   :forces forces
+   :liquid liquid})
 
-(defn draw-state [{balls :balls}]
+(defn draw-state [{balls :balls liquid :liquid}]
   (q/background 240)
+  (q/fill 50 50 200)
+  (q/rect
+    (get-in liquid [:bounds :x :min])
+    (get-in liquid [:bounds :y :min])
+    (get-in liquid [:bounds :x :max])
+    (get-in liquid [:bounds :y :max]))
   (q/fill 220 30 30)
   (doseq [ball balls]
     (let [location (:location ball)
