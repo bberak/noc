ad175530e262a29081849f59a10395e7f47bc10a
Boris Berak <bberak@gmail.com>
2016-03-28 17:14:32 +1100

    Worked on making the animation smoother (or at least, controllable)

--- simple-harmonic-motion/src/simple_harmonic_motion/core.clj
+++ simple-harmonic-motion/src/simple_harmonic_motion/core.clj
@@ -10,24 +10,35 @@
 (def frequency (/ 1 period)) ;; Waves per frame
 (def wavelength (/ speed frequency)) ;; Pixels
 (def amplitude 80)
+(def samples-per-frame 6)
 
 (defn setup []
   (q/frame-rate 30)
-  {:location (v/create 0 0)})
+  {:samples []
+   :frame 0})
 
-(defn update-state [{location :location}]
-  (let [frame-count (q/frame-count)
-        x (* frame-count speed)
-        y (* amplitude (q/sin (* two-pi (/ frame-count period))))]
-        {:location (v/create x y)}))
+(defn update-state [{samples :samples frame :frame}]
+  (let [start (* (dec frame) speed)
+        end (+ start speed)
+        sample-step (/ (- end start) samples-per-frame)
+        sample-range (range start end sample-step)]
+        {:samples (concat samples (map (fn [x] (v/create x (* amplitude (q/sin (* two-pi (/ x period)))))) sample-range))
+         :frame (inc frame)}))
 
-(defn draw-state [{location :location}]
+(defn draw-state [{samples :samples}]
   (q/background 240)
   (q/fill 0 0 0)
-  (q/text (str "Period: " period " frames\nSpeed: " speed " pixels per frame\nFrequency: " frequency " waves per frame\nWavelength: " wavelength " pixels\nAmplitude: " amplitude) 10 50)
+  (q/text (str 
+    "Period: " period " frames\n"
+    "Speed: " speed " pixels per frame\n"
+    "Frequency: " frequency " waves per frame\n"
+    "Wavelength: " wavelength " pixels\n"
+    "Amplitude: " amplitude "\n"
+    "Samples: " samples-per-frame " samples per frame") 10 50)
   (q/fill 0 255 255)
   (q/with-translation [0 (/ (q/height) 2)]
-    (q/ellipse (:x location) (:y location) 10 10)))
+    (doseq [sample samples]
+      (q/ellipse (:x sample) (:y sample) 10 10))))
 
 (defn -main []
   (q/defsketch simple-harmonic-motion
