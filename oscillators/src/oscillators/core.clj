34a8af8a33f6e51389cb384facb3ecb87468bf2e
bberak <bberak@gmail.com>
2016-03-29 20:24:37 +1100

    Added some angular acceleration into the mix (oscillators example)

--- oscillators/src/oscillators/core.clj
+++ oscillators/src/oscillators/core.clj
@@ -4,9 +4,12 @@
             [oscillators.vectors :as v]
             [oscillators.forces :as f]))
 
+(def max-angular-velocity 0.5)
+
 (defn create-oscillator []
   {:angle (v/create 0 0)
    :angular-velocity (v/create (q/random -0.05 0.05) (q/random -0.05 0.05))
+   :angular-acceleration (v/create (q/random -0.0015 0.0015) (q/random -0.0015 0.0015))
    :amplitude (v/create (/ (q/width) 4) (/ (q/height) 4))
    :location (v/create 0 0)})
 
@@ -15,10 +18,11 @@
   {:oscillator (create-oscillator)})
 
 (defn update-state [{oscillator :oscillator}]
-  (let [new-angle (v/add (:angle oscillator) (:angular-velocity oscillator))
+  (let [new-angular-velocity (v/constrain-magnitude (v/add (:angular-velocity oscillator) (:angular-acceleration oscillator)) max-angular-velocity)
+        new-angle (v/add (:angle oscillator) new-angular-velocity)
         x (* (get-in oscillator [:amplitude :x]) (q/sin (:x new-angle)))
         y (* (get-in oscillator [:amplitude :y]) (q/sin (:y new-angle)))]
-    {:oscillator (assoc oscillator :angle new-angle :location (v/create x y))}))
+    {:oscillator (assoc oscillator :angle new-angle :angular-velocity new-angular-velocity :location (v/create x y))}))
 
 (defn draw-state [{oscillator :oscillator}]
   (q/background 240)
