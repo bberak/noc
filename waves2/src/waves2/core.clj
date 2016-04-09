cec31f400c0070997705a3a2cba2f659585a930a
bberak <bberak@gmail.com>
2016-04-09 19:25:41 +1000

    Added an 'additive' style wave and also one that is calculated using Perlin noise

--- waves2/src/waves2/core.clj
+++ waves2/src/waves2/core.clj
@@ -7,17 +7,30 @@
 
 (defn setup []
   (q/frame-rate 60)
-  {:waves [;;(assoc (w/sine-wave (v/create 50 100) 400 150 15 1.95 25 0) :render w/render)
-           ;;(assoc (w/cosine-wave (v/create 50 400) 400 250 30 0.95 20 -0.5) :render #(w/render-ellipses % 50 50 50 50 50 120))
-           ;;(assoc (w/cosine-wave (v/create 350 400) 400 250 30 0.95 5 -0.25) :render #(w/render-rects % 50 50 50 50 50 120))
-           ;;(assoc (w/cosine-wave (v/create 300 100) 400 250 45 0.95 20 0) :render #(w/render-rects-with-heading % 40 40 50 50 50 120))        
-           (assoc (w/add 
-             (w/sine-wave (v/create 0 0) 100 500 15 0.05 15 0)
-             (w/cosine-wave (v/create 0 0) 10 2 3 0.05 10 0)
-             (w/sine-wave (v/create 0 0) 50 15 5 0.05 15 0)
-             (w/cosine-wave (v/create 0 0) 40 20 3 0.05 10 0)
-             (w/sine-wave (v/create 50 200) 100 10 15 0.05 15 0))
+  {:waves [(assoc (w/sine-wave (v/create 50 100) 400 150 15 1.95 25 0) :render w/render)
+           (assoc (w/cosine-wave (v/create 100 450) 400 250 30 0.95 20 -0.5) :render #(w/render-ellipses % 25 25 50 50 50 120))
+           (assoc (w/cosine-wave (v/create 350 400) 400 250 30 0.95 5 -0.25) :render #(w/render-rects % 50 50 50 50 50 120))
+           (assoc (w/cosine-wave (v/create 400 100) 400 250 45 0.95 20 0) :render #(w/render-rects-with-heading % 40 40 50 50 50 120))        
+           ;; Moving wave that created with several sine and cosine functions
+           (assoc 
+             (w/wave (v/create 10 500) 400 150 15 0.95 45 1 (fn [amp theta period]
+                                                                (+ 
+                                                                  (* (* amp (q/sin (* w/two-pi (/ theta period)))) 0.65)
+                                                                  (+ (* amp (q/cos (* w/two-pi (/ theta period)))) (- 22 period))
+                                                                  (- (* amp (q/sin (* w/two-pi (/ theta period)))) (* theta 2))
+                                                                  (/ (* amp (q/cos (* w/two-pi (/ theta period)))) amp)
+                                                                  (* amp (q/sin (* w/two-pi (/ theta period)))))))
+             :render #(w/render-ellipses % 50 50 50 50 50 120))   
+           ;; Organic wave created using perlin noise
+           (assoc 
+             (w/wave (v/create 60 200) 400 150 15 0.95 45 0 (fn [amp theta period]
+                                                              (q/noise-detail 1)
+                                                              (let [noise (q/noise (* theta 0.01))]
+                                                                  (* (* amp noise 8) (q/sin (* w/two-pi (/ theta period)))                       
+                                                                ))))
              :render #(w/render-ellipses % 50 50 50 50 50 120))
+           
+           
            ]})
 
 (defn update-state [{waves :waves}]
@@ -32,7 +45,7 @@
 (defn -main []
   (q/defsketch waves2
     :title "Waves 2"
-    :size [640 480]
+    :size [840 480]
     :setup setup
     :update update-state
     :draw draw-state
