e34ce09af95ef9d82132e44482ce4bac85b1e1af
Boris Berak <bberak@gmail.com>
2016-05-10 00:49:34 +1000

    Continued work on the basic pendulum example

--- pendulum/src/pendulum/core.clj
+++ pendulum/src/pendulum/core.clj
@@ -3,37 +3,40 @@
             [quil.middleware :as m]
             [pendulum.vectors :as v]))
 
-(def gravity 8)
+(def gravity 0.4)
 
-(defn create-pendulum [r angle angular-velocity mass]
+(defn create-pendulum [r angle angular-velocity]
   {:r r
    :angle angle
-   :angular-velocity angular-velocity
-   :mass mass})
+   :angular-velocity angular-velocity})
 
 (defn setup []
   (q/frame-rate 30)
-  {:pendulum (create-pendulum 300 (q/radians 60) 0 5)})
+  (q/stroke 0)
+  (q/stroke-weight 2)
+  (q/fill 120 120 120)
+  {:pendulum (create-pendulum 175 (q/radians 60) 0)})
 
 (defn update-pendulum [p]
-  (let [force (* -1 (* gravity (q/sin (:angle p))))
-        angular-acceleration (/ force (:mass p))
-        new-angular-velocity (+ angular-acceleration (:angular-velocity p))
+  (let [angular-acceleration (/ (* -1 (* gravity (q/sin (:angle p)))) (:r p))
+        damping 0.995
+        new-angular-velocity (* (+ angular-acceleration (:angular-velocity p)) damping)
         new-angle (+ new-angular-velocity (:angle p))]
-    (assoc p :angular-velocity new-angular-velocity :angle new-angular-velocity)))
+    (assoc p :angular-velocity new-angular-velocity :angle new-angle)))
 
 (defn update-state [{p :pendulum}]
   {:pendulum (update-pendulum p)})
 
 (defn draw-state [{p :pendulum}]
   (q/background 240)
-  (q/fill 120 255 255)
   (let [origin (v/create (/ (q/width) 2) 75)
         theta (:angle p)
-        r (:r p)]
-    (q/with-translation [(/ (q/width) 2)
-                         (/ (q/height) 2)]
-      (q/ellipse x y 100 100))))
+        r (:r p)
+        location (v/add origin
+          (v/create (* r (q/sin theta))(* r (q/cos theta))))]
+        (q/line (:x origin) (:y origin) (:x location) (:y location))
+        (q/ellipse (:x origin) (:y origin) 10 10)
+        (q/ellipse (:x location) (:y location) 50 50)))
 
 (defn -main [] 
   (q/defsketch pendulum
