2e1dc78e2b7441851771ae1dd655c061fdeb89e0
bberak <bberak@gmail.com>
2016-05-26 22:42:53 +1000

    Continued work on the spring forces example

--- spring-forces/src/spring_forces/core.clj
+++ spring-forces/src/spring_forces/core.clj
@@ -16,34 +16,38 @@
   (q/stroke 0)
   (q/stroke-weight 2)
   (q/fill 120 120 120)
-  {:spring (create-spring (v/create (/ (q/width) 2) 75) 350 0.1)})
+  (q/rect-mode :center)
+  {:spring (create-spring (v/create (/ (q/width) 2) 75) 350 0.1)
+   :gravity (v/create 0 1)})
 
-(defn update-spring [spring]
+(defn update-spring [spring gravity]
   (let [anchor (:anchor spring)
-        rigidity (:rigidity spring)
         bob (:bob spring)
         bob-location (:location bob)
         bob-velocity (:velocity bob)
         bob-mass (:mass bob)
+        rigidity (:rigidity spring)
         rest-length (:rest-length spring)
         direction (v/subtract bob-location anchor)
-        current-length (v/distance anchor bob-location)
-        diff (- current-length rest-length)
-        spring-force (* -1 rigidity diff)
-        force-vector (v/divide (v/multiply (v/normalize direction) spring-force) bob-mass)
-        new-velocity (v/add bob-velocity force-vector)
+        current-length (v/magnitude direction)
+        stretch (- current-length rest-length)
+        spring-force (v/multiply (v/normalize direction) (* -1 rigidity stretch))
+        forces (v/divide (v/add spring-force gravity) bob-mass)
+        damping 0.99
+        new-velocity (v/multiply (v/add bob-velocity forces) damping)
         new-location (v/add bob-location new-velocity)]
     (update-in spring [:bob] assoc :location new-location :velocity new-velocity)))
 
-(defn update-state [{spring :spring}]
-  {:spring (update-spring spring)})
+(defn update-state [{spring :spring gravity :gravity}]
+  {:spring (update-spring spring gravity)
+   :gravity gravity})
 
 (defn draw-state [{spring :spring}]
   (q/background 240)
   (let [anchor (:anchor spring)
         bob (:bob spring)]
     (q/line (:x anchor) (:y anchor) (get-in bob [:location :x]) (get-in bob [:location :y]))
-    (q/ellipse (:x anchor) (:y anchor) 35 35)
+    (q/rect (:x anchor) (:y anchor) 25 25)
     (q/ellipse (get-in bob [:location :x]) (get-in bob [:location :y]) (* (:mass bob) 10) (* (:mass bob) 10))))
 
 (defn -main []
