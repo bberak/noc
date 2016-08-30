6a548a4c8c5b53ab81f6acd359391a770071f811
bberak <bberak@gmail.com>
2016-08-31 01:06:32 +1000

    Added the box2d surfaces project

--- chapter-05/surfaces/src/surfaces/core.clj
+++ chapter-05/surfaces/src/surfaces/core.clj
@@ -1,27 +1,21 @@
-(ns boxy-press.core
+(ns surfaces.core
   (:require [quil.core :as q]
             [quil.middleware :as m]
             [basic-ces.core :refer :all]
-            [boxy-press.systems :refer :all]
+            [surfaces.systems :refer :all]
             [org.nfrac.cljbox2d.testbed :as tb]
             [org.nfrac.cljbox2d.core :refer :all]
             [org.nfrac.cljbox2d.vec2d :refer :all]))
 
 (def fps 60)
 
-(defn render-platform [camera platform-components]
-  (let [body (:body platform-components)
-        angle (* (angle body) -1)
-        world-pos (position body)
-        px-pos (tb/world-to-px camera world-pos)
-        width (:width platform-components)
-        height (:height platform-components)
-        scale (tb/world-to-px-scale camera)]
-    (q/fill 100)
-    (q/rect-mode :center)
-    (q/with-translation px-pos
-      (q/with-rotation [angle]
-        (q/rect 0 0 (* width scale) (* height scale))))))
+(defn render-chain [camera chain-components]
+  (q/no-fill)
+  (q/begin-shape)
+    (doseq [vertex (:vertices chain-components)]
+      (let [[x y] (tb/world-to-px camera vertex)]
+        (q/curve-vertex x y)))
+  (q/end-shape))
 
 (defn render-ball [camera ball-components]
   (let [body (:body ball-components)
@@ -36,8 +30,8 @@
 (defn create-ball [world pos]
   (entity {:label :ball
            :renderable render-ball
-           :radius 1
-           :body (body! world {:position pos} {:shape (circle 1) :restitution 0.7})}))
+           :radius 0.5
+           :body (body! world {:position pos} {:shape (circle 0.5) :restitution 0.7})}))
 
 (defn render-box [camera box-components]
   (let [body (:body box-components)
@@ -55,37 +49,26 @@
 (defn create-box [world pos]
   (entity {:label :ball
            :renderable render-box
-           :length 2
-           :body (body! world {:position pos} {:shape (box 1 1) :restitution 0.7})}))
+           :length 1
+           :body (body! world {:position pos} {:shape (box 0.5 0.5) :restitution 0.7})}))
 
 (defn setup []
   (q/frame-rate fps)
+  (q/noise-detail 2)
   (let [camera {:width 40
                 :height 30
                 :center [20 15]}
-        world (new-world [0 -10])]
+        world (new-world [0 -10])
+        vertices (map (fn [x] [x (* (q/noise (/ x 5)) 20)]) (range 40))]
     (merge {}
            (entity {:label :camera
                     :camera camera}) 
            (entity {:label :world
                     :world world})
-           (entity {:label :platform
-                    :renderable render-platform
-                    :width 10
-                    :height 0.5
-                    :body (body! world {:position [30 25] :type :static :angle (q/radians 45)} {:shape (box 5 0.25) :restitution 0.7})})
-           (entity {:label :platform
-                    :renderable render-platform
-                    :width 10
-                    :height 0.5
-                    :body (body! world {:position [20 15] :type :static} {:shape (box 5 0.25) :restitution 0.7})})
-           (entity {:label :platform
-                    :renderable render-platform
-                    :width 30
-                    :height 0.5
-                    :body (body! world {:position [20 5] :type :static} {:shape (box 15 0.25) :restitution 0.7})})
-           (create-box world [25.5 25])
-           (create-ball world [14.5 25]))))
+           (entity {:label :chain
+                    :renderable render-chain
+                    :vertices vertices
+                    :body (body! world {:type :static} {:shape (edge-chain vertices) :restitution 0.7})}))))
 
 (defn prog-loop [entities]
   (q/background 240)
@@ -95,9 +78,9 @@
       (right-click-and-spawn-box create-box)
       (renderer)))
 
-(defn -main [] 
-  (q/defsketch boxy-press
-    :title "Press for boxes!"
+(defn -main []
+  (q/defsketch surfaces
+    :title "Surfaces"
     :size [800 600]
     :setup setup
     :update prog-loop
---
+++ chapter-05/surfaces/src/surfaces/systems.clj
@@ -0,0 +1,39 @@
+(ns surfaces.systems
+  (:require [quil.core :as q]
+            [quil.middleware :as m]
+            [basic-ces.core :refer :all]
+            [org.nfrac.cljbox2d.testbed :as tb]
+            [org.nfrac.cljbox2d.core :refer :all]
+            [org.nfrac.cljbox2d.vec2d :refer :all]))
+
+(defn ticker [entities secs]
+  (doseq [[id components] (filter-entities entities :world)]
+    (let [world (:world components)]
+      (step! world secs)))
+  entities)
+
+(defn left-click-and-spawn-ball [entities create-ball-func]
+  (if (and (q/mouse-pressed?) (= (q/mouse-button) :left))
+    (let [world (:world (second (first (filter-entities entities :world))))
+          camera (:camera (second (first (filter-entities entities :camera))))
+          mouse-pos [(q/mouse-x) (q/mouse-y)]
+          world-pos (tb/px-to-world camera mouse-pos)]
+      (merge entities (create-ball-func world world-pos)))
+    entities))
+
+(defn right-click-and-spawn-box [entities create-box-func]
+  (if (and (q/mouse-pressed?) (= (q/mouse-button) :right))
+    (let [world (:world (second (first (filter-entities entities :world))))
+          camera (:camera (second (first (filter-entities entities :camera))))
+          mouse-pos [(q/mouse-x) (q/mouse-y)]
+          world-pos (tb/px-to-world camera mouse-pos)]
+      (merge entities (create-box-func world world-pos)))
+    entities))
+  
+
+(defn renderer [entities]
+  (let [camera (:camera (second (first (filter-entities entities :camera))))]
+    (doseq [[id components] (filter-entities entities :renderable)]
+      (let [render-func (:renderable components)]
+        (render-func camera components)))
+    entities))
